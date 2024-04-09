09c72c0acd627643d9ae47713df0394481846e2a
paola2262 <159589418+paola2262@users.noreply.github.com>
2024-04-09 11:12:30 -0500

    main

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "adde4b60d3947f9183badbff678a2426"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "6647306744d34907b0357932865cc014"
